260dcd2f5e3cb062ca64045665fb4af4f44ec304
rkreddy99 <krishna.reddy@iitgn.ac.in>
2020-12-26 12:52:08 +0530

    Checkout page

---
+++ src/CheckoutProduct.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./CheckoutProduct.css";
+import { useStateValue } from "./StateProvider";
+
+function CheckoutProduct({ id, image, title, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const removeFromBasket = () => {
+    // remove from the basket
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  };
+  return (
+    <div className="checkoutProduct">
+      <img className="checkoutProduct__image" src={image} alt="" />
+
+      <div class="checkoutProduct__info">
+        <p class="checkoutProduct__title">{title}</p>
+        <p class="checkoutProduct__price">
+          <small>&#8377;</small>
+          <strong>{price}</strong>
+        </p>
+        <div class="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
+        </div>
+        <button onClick={removeFromBasket}>Remove from basket</button>
+      </div>
+    </div>
+  );
+}
+
+export default CheckoutProduct;
