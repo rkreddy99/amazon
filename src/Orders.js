f1b13a5864e1ff820a56aa83c74908fae180bbae
rkreddy99 <krishna.reddy@iitgn.ac.in>
2021-01-05 10:58:11 +0530

    Stripe Payment

---
+++ src/Orders.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { useStateValue } from "./StateProvider";
+import { db } from "./firebase";
+import Order from "./Order";
+
+function Orders() {
+  const [orders, setOrders] = useState([]);
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    if (user) {
+      db.collection("users")
+        .doc(user?.uid)
+        .collection("orders")
+        .orderBy("created", "desc")
+        .onSnapshot((snapshot) => {
+          // console.log(snapshot.docs);
+          setOrders(
+            snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() }))
+          );
+        });
+      // console.log(orders);
+      // console.log(orders);
+    } else {
+      setOrders([]);
+    }
+  }, [user]);
+  return (
+    <div className="orders">
+      <h1>Your Orders</h1>
+      <div className="orders__order">
+        {orders?.map((order) => {
+          console.log(order);
+          return <Order order={order} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Orders;
